2fa37fba8188672bbe9fbb82a546cf2b1a3e5eeb
Игорь Морев <igormorev@Igors-MacBook-Pro.local>
2023-03-04 14:26:52 +0700

    prepared mf-host

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: MainComponent
+}, {
+  path: 'plan',
+  loadChildren: () => import('mfe1/Module').then(m => m.TableModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
