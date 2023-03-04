9c4c039a13bfe99ade6937d056f3a25025f98b3b
Игорь Морев <igormorev@Igors-MacBook-Pro.local>
2023-03-04 14:35:20 +0700

    add loading remote module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main/main.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 const routes: Routes = [{
   path: '',
   component: MainComponent
 }, {
   path: 'plan',
-  loadChildren: () => import('mfe1/Module').then(m => m.TableModule)
+  // loadChildren: () => import('mfe1/Module').then(m => m.TableModule),
+  loadChildren: () =>
+  loadRemoteModule({
+      type: 'module',
+      remoteEntry: 'http://localhost:4201/remoteEntry.js',
+      exposedModule: './Module'
+  })
+  .then(m => m.TableModule)
 }];
 
 @NgModule({
